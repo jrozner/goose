17c8293b83041113a174ca4dbf32dadf6f38d099
Joe Rozner <joe@deadbytes.net>
2015-03-29 19:06:00 -0700

    Add token to string lookup

--- ast.go
+++ ast.go
@@ -7,6 +7,35 @@ type Node struct {
 
 type NodeType uint
 
+var Nodes = map[NodeType]string{
+	NodeAddColumn:         "AddColumn",
+	NodeAddIndex:          "AddIndex",
+	NodeBoolean:           "Boolean",
+	NodeChangeColumn:      "ChangeColumn",
+	NodeColumnDefinition:  "ColumnDefinition",
+	NodeColumnName:        "ColumnName",
+	NodeCreateTable:       "CreateTable",
+	NodeDataType:          "DataType",
+	NodeDefaultValue:      "DefaultValue",
+	NodeDownStatement:     "DownStatement",
+	NodeIndexOptions:      "IndexOptions",
+	NodeIndexOptionsBlock: "IndexOptionsBlock",
+	NodeNewName:           "NewName",
+	NodeOption:            "Option",
+	NodeOptionsBlock:      "OptionsBlock",
+	NodeRanameColumn:      "RenameColumn",
+	NodeRaw:               "Raw",
+	NodeRawBody:           "RawBody",
+	NodeRemoveColumn:      "RemoveColumn",
+	NodeRemoveIndex:       "RemoveIndex",
+	NodeRemoveTable:       "RemoveTable",
+	NodeRenameTable:       "RenameTable",
+	NodeRoot:              "Root",
+	NodeStatement:         "Statement",
+	NodeTableName:         "TableName",
+	NodeUpStatement:       "UpStatement",
+}
+
 const (
 	NodeAddColumn NodeType = iota
 	NodeAddIndex
